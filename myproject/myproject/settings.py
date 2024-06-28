6b6fbba6d5a05f2daa87e970d7c673e692f33412
Umar Masood <99263424+umerpaknamal@users.noreply.github.com>
2024-06-28 16:24:51 +0500

    Update settings.py

--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -15,6 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# settings.py
+
+# Increase the maximum size for request body
+DATA_UPLOAD_MAX_MEMORY_SIZE = 25 * 1024 * 1024  # 10 MB
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
