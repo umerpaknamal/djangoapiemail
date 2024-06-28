2c63dc31e96a6c8c61ad42ad60bb896fabbdde76
Umar Masood <99263424+umerpaknamal@users.noreply.github.com>
2024-06-28 15:37:35 +0500

    Update urls.py

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import concatenate_email
-from . import views
+from .views import base64_view
 urlpatterns = [
     path('concatenate-email/', concatenate_email),
-    path('base64/', views.Base64View.as_view(), name='base64'),
+    path('base64/', base64_view, name='base64'),
 ]
