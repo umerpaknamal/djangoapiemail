3e87b42ee46040b2dfdf0172f21ec54422cd43d7
Umar Masood <99263424+umerpaknamal@users.noreply.github.com>
2024-07-08 21:49:12 +0500

    Update views.py

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -31,10 +31,11 @@ def base64_view(request):
 @api_view(['POST'])
 def concatenate_email(request):
     data = request.data
-
+ 
     access_token = data.get('access_token', '')
     thread_id = data.get('threadId', '')
-
+    original_message_id = data.get('originalMessageId', '')
+ 
     to = data.get('to', [])
     sender = data.get('sender', '')
     bcc = data.get('bcc', [])
@@ -44,13 +45,13 @@ def concatenate_email(request):
     encoded_attachments = data.get('encodedAttachments', [])
     attachment_name = data.get('attachmentname', [])
     attachment_type = data.get('attachmenttype', [])
-
+ 
     to_str = ", ".join(to) if isinstance(to, list) else to
     bcc_str = ", ".join(bcc) if isinstance(bcc, list) else bcc
     cc_str = ", ".join(cc) if isinstance(cc, list) else cc
-
-    boundary = "boundary_string"
-
+ 
+    boundary = "----=_Part_0_123456789.123456789"
+ 
     str_parts = [
         "Content-Type: multipart/mixed; boundary=" + boundary + "\n",
         "MIME-Version: 1.0\n",
@@ -58,40 +59,50 @@ def concatenate_email(request):
         "Bcc: {}\n".format(bcc_str),
         "Cc: {}\n".format(cc_str),
         "From: {}\n".format(sender),
-        "Subject: {}\n\n".format(subject),
+        "Subject: {}\n".format(subject),
+    ]
+ 
+    if original_message_id:
+        str_parts.extend([
+            "In-Reply-To: {}\n".format(original_message_id),
+            "References: {}\n".format(original_message_id),
+        ])
+ 
+    str_parts.extend([
         "--" + boundary + "\n",
         "Content-Type: text/html; charset=\"UTF-8\"\n",
         "Content-Transfer-Encoding: 7bit\n\n",
         message, "\n",
-    ]
-
-    for attachment_data, attachment_name, attachment_type in zip(encoded_attachments, attachment_name, attachment_type):
-        filename = attachment_name + "." + attachment_type
+    ])
+ 
+    for attachment_data, name, type in zip(encoded_attachments, attachment_name, attachment_type):
+        filename = name + "." + type
         str_parts.extend([
             "--" + boundary + "\n",
-            "Content-Type: application/{}\n".format(attachment_type),
+            "Content-Type: application/{}\n".format(type),
             "Content-Disposition: attachment; filename=\"{}\"\n".format(filename),
             "Content-Transfer-Encoding: base64\n\n",
             attachment_data, "\n",
         ])
-
+ 
     str_parts.append("--" + boundary + "--")
     email_body = "".join(str_parts)
     encoded_mail = base64.urlsafe_b64encode(email_body.encode()).decode()
-
+ 
     # Send the email using Gmail API
     credentials = Credentials(token=access_token)
     service = build('gmail', 'v1', credentials=credentials)
-
+ 
     message_data = {
         'raw': encoded_mail
     }
     if thread_id:
         message_data['threadId'] = thread_id
-
+ 
     try:
         message = service.users().messages().send(userId='me', body=message_data).execute()
         message_id = message['id']
-        return Response({'messageId': message_id})
+        thread_id = message['threadId']
+        return Response({'messageId': message_id, 'threadId': thread_id})
     except Exception as e:
         return Response({'error': str(e)}, status=400)
